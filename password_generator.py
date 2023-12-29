1d5392dd485702643a54aa43eae90ad3ce7b35c1
Andy <89641810+Andy00L@users.noreply.github.com>
2023-12-29 14:35:32 -0500

    Update password_generator.py
    Version plus optimiser, 
    - Ajout de le verification des entrer.
    - Optimization de la creation des mots de passe ( les rendant beaucoup plus aléatoire).

--- password_generator.py
+++ password_generator.py
@@ -9,37 +9,42 @@ def genere_mdp(min_longueur, nombre, caratere_special):
     chiffre = string.digits
     special = string.punctuation
     
-    caracteres = lettres
-    if nombre:
-        caracteres += chiffre
-    if caratere_special:
-        caracteres += special
-    
     mot_de_passe = "" 
-    valid_criteres = False
-    possede_chiffre = False
-    possede_special = False
     
-    while not valid_criteres or len(mot_de_passe) < min_longueur:
-        nouv_char = random.choice(caracteres)
+    while len(mot_de_passe) < min_longueur:
+        nouv_char = random.choice(lettres)
         mot_de_passe += nouv_char
         
-        if nouv_char in chiffre:
-            possede_chiffre = True
-        elif nouv_char in special:
-            possede_special = True
-            
-        valid_criteres = True
         if nombre:
-            valid_criteres = possede_chiffre
+            nouv_char = random.choice(chiffre)
+            mot_de_passe += nouv_char
+            
         if caratere_special:
-            valid_criteres = valid_criteres and possede_special
+            nouv_char = random.choice(special)
+            mot_de_passe += nouv_char
             
     return mot_de_passe
 
-min_longueur = int(input("Entrer la longueur minimale: "))
-possede_chiffre = str(input(" Tu veux des nombres (y/n)? ")).lower == "y"
-possede_special = str(input("Tu veux des carateres speciaux (y/n)? ")).lower == "y"
-pwd = genere_mdp(min_longueur, possede_chiffre, possede_special)
+# Verifie qu'on a rentrer un chiffre
+def chiffre(chr):
+    while not chr.isdigit():
+        chr = input("Veillez rentrez un chiffre: ")
+    return int(chr)
 
-print(" Voici le mot de passe generer:", pwd)
+# Verifie qu'on a rentrer soit un "y" ou un "n"
+def lettre(lettre):
+    while not lettre.isalpha() or lettre.lower() not in ["y","n"]:
+        lettre = input("(y/n): ")
+        
+    if lettre == "y":
+        return True
+    else:
+        return False
+    
+min_longueur = chiffre(chr = input("Entrer la longueur minimale: "))
+possede_chiffre = lettre(lettre = input(" Tu veux des nombres (y/n)? "))
+possede_special = lettre(lettre = input("Tu veux des carateres speciaux (y/n)? "))
+
+critere = genere_mdp(min_longueur, possede_chiffre, possede_special)
+
+print(" Voici le mot de passe generer:", critere)
